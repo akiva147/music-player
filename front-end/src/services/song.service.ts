2546417cdf800b20fb2aea4d0606ce4463da3ff7
Akiva levi <levi.akiva@agamimp.net>
2024-02-28 22:34:52 +0200

    fix(song): fixed the song services

--- front-end/src/services/song.service.ts
+++ front-end/src/services/song.service.ts
@@ -1,16 +1,24 @@
+import { SongOption, SongOptionSchema } from 'src/models/deezer'
 import { axiosInstance } from './axios.service'
 
 export class SongService {
     async getAutoCompleteResults(query: string) {
         const limit = 10
         try {
-            const res = await axiosInstance.get('/song', {
-                params: {
-                    query,
-                    limit,
-                },
-            })
-            console.log('🚀 ~ SongService ~ getAutoCompleteResults ~ res:', res)
+            const res = (
+                await axiosInstance.get<SongOption[]>('/song', {
+                    params: {
+                        query,
+                        limit,
+                    },
+                })
+            ).data
+
+            if (res.length === 0) return 'results not found'
+
+            const autoCompleteResults = SongOptionSchema.array().parse(res)
+
+            return autoCompleteResults
         } catch (error: unknown) {
             console.error(error)
             throw new Error(String(error))
